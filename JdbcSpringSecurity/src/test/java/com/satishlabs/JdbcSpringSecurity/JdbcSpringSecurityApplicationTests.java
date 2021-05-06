4ed36bca3e6cfbde0d98a2397b231c4724ef0871
Satish <spd61185@gmail.com>
2021-05-06 10:44:18 +0530

    Spring Security with JDBC -H2 Database

---
+++ JdbcSpringSecurity/src/test/java/com/satishlabs/JdbcSpringSecurity/JdbcSpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.satishlabs.JdbcSpringSecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JdbcSpringSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
